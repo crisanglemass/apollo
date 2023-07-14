27a49a357750fba1ec38fa1815b44ef7c870e9f0
crisangle <1875574446@qq.com>
2023-07-14 16:15:20 +0800

    Initial commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/7/14 14:56:54
+# @Author : Hui,Zhang
+# @File : demo.py
+# @Work email(cn) : 2222204003@stmail.ujs.edu.cn
+# @Work email(uk) : hui.zhang.353@cranfield.ac.uk
+# @Personal email : lanettebirkenholzvgh75@gmail.com
+import  os
+
+path='/data/data/name'
+print(os.path.dirname(path))
