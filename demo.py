cceac11a81e09f0f3923819f05f5d904bb44b12d
crisanglemass <lanettebirkenholzvgh75@gmail.com>
2023-08-01 13:58:55 +0800

    first commit

--- demo.py
+++ demo.py
@@ -5,7 +5,3 @@
 # @Work email(cn) : 2222204003@stmail.ujs.edu.cn
 # @Work email(uk) : hui.zhang.353@cranfield.ac.uk
 # @Personal email : lanettebirkenholzvgh75@gmail.com
-import  os
-
-path='/data/data/name'
-print(os.path.dirname(path))
